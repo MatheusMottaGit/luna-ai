5e109aab0649bcc588d0a573c8d3a3483e84e2ef
Matheus Motta <matheusdomingues423@gmail.com>
2023-09-29 19:55:46 -0300

    building-components

--- web/src/components/empty-video.tsx
+++ web/src/components/empty-video.tsx
@@ -2,7 +2,7 @@ import { FileVideo } from 'lucide-react'
 
 export const EmptyVideo = () => {
   return (
-    <div className='flex flex-col h-full items-center justify-center gap-2 bg-zinc-900 text-zinc-700 text-sm border border-dashed rounded-sm'>
+    <div className='flex flex-col h-[400px] items-center justify-center gap-2 text-zinc-700 text-sm border border-dashed rounded-sm'>
       <FileVideo className='h-4 w-4' />
 
       Preview do vídeo
