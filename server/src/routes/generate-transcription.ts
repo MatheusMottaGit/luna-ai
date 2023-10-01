114b9f8b4e0940f1429e490c11f65924b33fae2d
Matheus Motta <matheusdomingues423@gmail.com>
2023-10-01 20:37:43 -0300

    building-transcription-route

--- server/src/routes/generate-transcription.ts
+++ server/src/routes/generate-transcription.ts
@@ -1,7 +1,51 @@
 import { FastifyInstance } from "fastify";
+import { z } from "zod";
+import { prisma } from "../lib/prisma";
+import { openai } from "../lib/openai";
+import { createReadStream } from "fs";
 
 export async function generateTranscriptionRoute(app: FastifyInstance) {
-  app.post('video/:videoId/transcription', async (request) => {
+  app.post('/video/:videoId/transcription', async (request) => {
+    const paramsSchema = z.object({
+      videoId: z.string().uuid()
+    })
 
+    const { videoId } = paramsSchema.parse(request.params)
+
+    const bodySchema = z.object({
+      prompt: z.string()
+    })
+
+    const { prompt } = bodySchema.parse(request.body)
+
+    const video = await prisma.video.findUniqueOrThrow({
+      where: {
+        id: videoId
+      }
+    })
+
+    const videoPath = video.path
+    const audioReadStream = createReadStream(videoPath)
+
+    const response = await openai.audio.transcriptions.create({
+      file: audioReadStream,
+      model: 'whisper-1',
+      prompt
+    })
+
+    const transcription = response.text
+
+    await prisma.video.update({
+      where: {
+        id: videoId
+      },
+
+      data: {
+        transcription
+      }
+    })
+
+    console.log(transcription)
+    return { transcription }
   })
 }
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,15 +1,19 @@
 import fastify from "fastify";
-import cors from '@fastify/cors'
 import { downloadVideoRoute } from "./routes/download-youtube-video";
 import 'dotenv/config'
+import { uploadRoute } from "./routes/upload-video";
+import { generateTranscriptionRoute } from "./routes/generate-transcription";
+import { fastifyCors } from "@fastify/cors";
 
 const app = fastify()
 
-app.register(cors, {
+app.register(fastifyCors, {
   origin: '*'
 })
 
 app.register(downloadVideoRoute)
+app.register(uploadRoute)
+app.register(generateTranscriptionRoute)
 
 app.listen({
   port: 3333
